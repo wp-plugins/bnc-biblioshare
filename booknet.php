20aa4d1f1c5c2d9b8d9e3a3e6e2ad9cd0f97b6e7
kirtim <kirtim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-12 20:24:53 +0000

    button fix 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/bnc-biblioshare/trunk@912932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- booknet.php
+++ booknet.php
@@ -3,7 +3,7 @@
 Plugin Name: BNC BiblioShare
 Plugin URI: http://wordpress.org/extend/plugins/bnc-biblioshare/
 Description: Displays a book's cover image, title, author, and other book data from BNC BiblioShare
-Version: 1.0.6
+Version: 1.0.7
 Author: BookNet Canada
 Author URI: http://www.booknetcanada.ca/
 
